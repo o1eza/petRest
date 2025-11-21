94778f544ab1dee39eaa8e8ae241aec95d865ae9
olehg <oromanuk626@gmail.com>
2025-11-21 18:06:42 +0100

    emptyArchitype

---
+++ src/main/java/com/PetProject/petRest/PetRestApplication.java
@@ -0,0 +1,13 @@
+package com.PetProject.petRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetRestApplication.class, args);
+	}
+
+}
